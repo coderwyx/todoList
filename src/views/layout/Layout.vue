41ad7784acc4a550e4bcd693ca82b455aaa8592e
王宇轩 <2514138002@qq.com>
2022-03-28 11:34:32 +0800

    样式修改

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -27,7 +27,7 @@
                 <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
             </a-layout-header>
             <a-layout-content
-                :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '640px', }"
+                :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px', }"
             >
             <router-view></router-view>
             </a-layout-content>
